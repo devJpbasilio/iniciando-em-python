43df96a565c2c716ddb32c220879c6a43d3bc052
João P Silva <jpsilva313@gmail.com>
2024-12-10 15:08:33 -0300

    add exs, add POO

--- POO/encapsulamento.py
+++ POO/encapsulamento.py
@@ -7,8 +7,32 @@ O encapsulamento controla o acesso aos atributos de um objeto:
 '''
 
 class ContaBancaria:
-    def __init__(self, saldo):
+    def __init__(self, titular, saldo):
+        self.titular = titular
         self.__saldo = saldo # Atributo privado
 
 
     def depositar(self, valor):
+        self. __saldo += valor
+        print(f'depósito de R$ {valor:.2f} realizado com sucesso')
+
+
+    def sacar(self, valor):
+        if valor <= self.__saldo:
+            self.__saldo -= valor
+            print(f'Saque de {valor:.2f} realizado com sucesso')
+        else:
+            print('Saldo insuficiente.')
+
+
+    def exibir_saldo(self):
+        print(f'Saldo Atual: {self.__saldo:.2f}')
+
+
+# Testando a classe
+
+conta = ContaBancaria("João", 1000)
+conta.exibir_saldo()
+conta.depositar(500)
+conta.sacar(250)
+conta.exibir_saldo()
